3beca915438124f3a8c1be21adbf39e0051741f3
quangtruong452001 <58112251+quangtruong452001@users.noreply.github.com>
2022-05-31 16:09:16 +0700

    Render for database

--- View/header.php
+++ View/header.php
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-
 <!doctype html>
 <html lang="en">
 
@@ -84,56 +81,3 @@
 
 
 
-=======
-<!--    Boostrap link-->
-<!--    CSS-->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<!--    JS-->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-<link rel="stylesheet" href="../styles/home.css">
-
-<section class="black-section">
-    <!--nav bar-->
-    <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <a class="navbar-brand" href="home.php"><img src="../images/logo.png"> Lgear </a>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Laptop
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Acer</a>
-                            <a class="dropdown-item" href="#">Asus</a>
-                            <a class="dropdown-item" href="#">MSI</a>
-                        </div>
-                    <li class="nav-item">
-                        <a class="nav-link" href="">Macbook</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="?page=signin">Sign In</a>
-
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="?page=signup">Sign Up</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="" title="View your shopping cart"><img src="../images/shopping-cart.png"></a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-
-
-
-</section>
->>>>>>> 71b57d56e8f81b31a8633013c13fede60d513ad6
